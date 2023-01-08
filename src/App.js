b554c6af786a9d846071d730286225f6139f724b
Davi Leungpw <davileung819@gmail.com>
2023-01-08 22:45:50 +0800

    share link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import icon from "./assets/icon.jpeg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+import AddButton from "./Components/AddBtn";
+import Linklist from "./Components/LinkList";
+import SearchBar from "./Components/SearchBar";
+
+export default function App() {
+  //  For List
+  const storedLinks = localStorage.getItem("links");
+  const parsedLinks =
+    storedLinks === "" || storedLinks === null ? [] : JSON.parse(storedLinks);
+
+  const [links, setLinks] = useState(
+    Array.isArray(parsedLinks) ? parsedLinks : []
+  );
+
+  //  For add button
+  const onAddButtonAddLink = (name, url, tags) => {
+    const newLinks = links.concat([
+      {
+        name,
+        url,
+        tags,
+      },
+    ]);
+    setLinks(newLinks);
+    localStorage.setItem("links", JSON.stringify(newLinks));
+  };
+
+  //  For delete button
+  const onDeleteOne = (i) => {
+    const deleteLinks = [...links];
+    deleteLinks.splice(i, 1);
+    setLinks(deleteLinks);
+    localStorage.setItem("links", JSON.stringify(deleteLinks));
+  };
+
+  //for search
+  const [search, setSearch] = useState("");
+  const onSearchBarChange = (search) => {
+    setSearch(search);
+  };
+
+  const filteredLinks = (search) => {
+    const lowerSearch = search.toLowerCase();
+    return links.filter((link) => {
+      return (
+        link.name.toLowerCase().indexOf(lowerSearch) > -1 ||
+        link.url.toLowerCase().indexOf(lowerSearch) > -1 ||
+        link.tags
+          .map((tag) => {
+            return tag.name.toLowerCase().indexOf(lowerSearch) > -1;
+          })
+          .indexOf(true) > -1
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="appcontainer">
+        <div className="Sidebar">
+          <img className="icon" src={icon} alt={icon} />
+          <h4 className="name">poodle's fav URL</h4>
+          <p>{links.length} shared links.</p>
+          <AddButton onAddLinkProps={onAddButtonAddLink} />
+        </div>
+        <div id="content_right">
+          <div id="header">
+            <SearchBar onSearchChangeProp={onSearchBarChange} />
+          </div>
+          <div id="main">
+            <Linklist
+              links={filteredLinks(search)}
+              deleteLink={(i) => onDeleteOne(i)}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
-export default App;
